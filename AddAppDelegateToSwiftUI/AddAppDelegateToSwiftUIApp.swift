5f173b9be67bf1d01e6a6bec4e58eae5d42eed17
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-31 09:12:14 +0300

    init

--- AddAppDelegateToSwiftUI/AddAppDelegateToSwiftUIApp.swift
+++ AddAppDelegateToSwiftUI/AddAppDelegateToSwiftUIApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct AddAppDelegateToSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        print("Finish launching!")
+        return true
     }
 }
