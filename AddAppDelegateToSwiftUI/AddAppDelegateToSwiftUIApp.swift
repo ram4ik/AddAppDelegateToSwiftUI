07b346c13bf29ebbc020a91a6332cb3c9ec93967
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-31 09:08:23 +0300

    Initial Commit

---
+++ AddAppDelegateToSwiftUI/AddAppDelegateToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddAppDelegateToSwiftUIApp.swift
+//  AddAppDelegateToSwiftUI
+//
+//  Created by ramil on 31.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct AddAppDelegateToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
